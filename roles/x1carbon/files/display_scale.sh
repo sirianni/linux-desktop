31b444438c0e86465c46137b3506d04f4bfca577
Eric Sirianni <eric.sirianni@gmail.com>
2018-10-26 10:24:45 -0400

    Disable display scaling for external monitor.

--- roles/x1carbon/files/display_scale.sh
+++ roles/x1carbon/files/display_scale.sh
@@ -19,32 +19,36 @@ if ! [[ $scale =~ ^[1-9](\.[0-9]*)?$ ]]; then
     exit 1
 fi
 
-# TO DO
-#gsettings set org.gnome.desktop.interface scaling-factor 2
-
 xrandr=$(xrandr -q)  # Get current screen properties
 
+monitorCount=$(grep ' connected' <<< $xrandr |
+               wc -l)
+
+# TODO Consider detecting this based on the gnome scaling-factor instead of external monitors
+# Will need to fix if I get a HiDPI external monitor :)
+if [[ $monitorCount == "2" ]] ; then
+    echo "Skipping fractional display scaling for external monitor"
+    exit 1
+fi
+
 # Get the current panning value (i.e. up/down scaled resolution):
-currentPan=$(xrandr |
-             grep -o "current[^,]\+" |  # Get "current [pan]" section
-             cut -c8- |                 # Remove "current" (first 8 chars)
-             tr -d "[:space:]")         # Remove all whitespace
+currentPan=$(grep -o "current[^,]\+" <<< $xrandr |  # Get "current [pan]" section
+             cut -c8- |                             # Remove "current" (first 8 chars)
+             tr -d "[:space:]")                     # Remove all whitespace
 
 # Get max res supported by hardware:
-res=$("xrandr" |
-      sed '3!d' |      # Get line 3 (should be the the correct line...)
-      xargs |          # Remove leading whitespace
-      grep -o '^\S*')  # Remove stuff after resolution (starting with 1st ' ')
+res=$(sed '3!d' <<< $xrandr |       # Get line 3 (should be the the correct line...)
+      xargs |                       # Remove leading whitespace
+      grep -o '^\S*')               # Remove stuff after resolution (starting with 1st ' ')
 
 # Get display identifier:
-display=$("xrandr" |
-          sed '2!d' |      # Get line 2 (should be the the correct line...)
-          grep -o '^\S*')  # Remove stuff after display (starting with 1st ' ')
+display=$(sed '2!d' <<< $xrandr |   # Get line 2 (should be the the correct line...)
+          grep -o '^\S*')           # Remove stuff after display (starting with 1st ' ')
 
-xRes=$(echo $res | cut -d "x" -f 1)
-yRes=$(echo $res | cut -d "x" -f 2)
-xPan=$(echo "scale=0;($xRes*$scale)/1" | bc)
-yPan=$(echo "scale=0;($yRes*$scale)/1" | bc)
+xRes=$(cut -d "x" -f 1 <<< $res)
+yRes=$(cut -d "x" -f 2 <<< $res)
+xPan=$(bc <<< "scale=0;($xRes*$scale)/1")
+yPan=$(bc <<< "scale=0;($yRes*$scale)/1")
 pan=$xPan'x'$yPan
 scale=$scale'x'$scale  # Change scale from "1.5" to "1.5x1.5"
 
