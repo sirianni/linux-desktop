b2b7277576f8ce02b734ab31d214467a1c7e11be
Eric Sirianni <eric.sirianni@gmail.com>
2018-10-26 20:16:03 -0400

    Update default scaling factor

--- roles/x1carbon/files/display_scale.sh
+++ roles/x1carbon/files/display_scale.sh
@@ -4,7 +4,7 @@
 
 # Modify this to change the default if you don't want to use the
 # optional argument. E.g: scale=${1:-1.25} for 1.25x scaling.
-scale=${1:-1.5}
+scale=${1:-1.4}
 
 echo "Running $(basename $0)"
 
